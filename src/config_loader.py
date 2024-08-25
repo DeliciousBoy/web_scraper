ff6ee49e67f84a71d545bb0e9a98cc20773ac786
Wittaya Tangdorn <83308832+DeliciousBoy@users.noreply.github.com>
2024-08-25 21:52:39 +0700

    Add config loader to load scraping configuration from YAML file - Implement ScrapeSelector and ProductElements dataclass to store scraping selectors. - Create load_config function to load configuration from a YAML file. - Handle file not found and YAML parsing errors with appropriate error messages.

--- src/config_loader.py
+++ src/config_loader.py
@@ -1,15 +1,26 @@
 import yaml
 from pathlib import Path
 from dataclasses import dataclass
-from typing import Union
+from typing import Union, Tuple, Optional
 
 @dataclass()
 class ScraperSelector:
+    """ Dataclass for storing Classname and XPATH selectors for scraping."""
     all_product_class: str
     product_list_class: str
     product_link_path: str
 
-def load_config(*, config_path: Union[str, Path]) -> ScraperSelector:
+@dataclass
+class ProductElements:
+    """ Dataclass for storing product element selectors."""
+    name: str
+    id: str
+    size: str
+    weight: str
+    price: str
+    unit: str
+
+def load_config(*, config_path: Union[str, Path]) -> Tuple[ScraperSelector, ProductElements, Optional[str]]:
     """
     Load the YAML configuration file and return a config object.
     """
@@ -23,8 +34,18 @@ def load_config(*, config_path: Union[str, Path]) -> ScraperSelector:
             product_list_class=config['selector']['product_list_class'],
             product_link_path=config['selector']['product_link_path'],
         )
-        
-        return selector
+
+        element = ProductElements(
+            name=config['product_element']['name'],
+            id=config['product_element']['id'],
+            size=config['product_element']['size'],
+            weight=config['product_element']['weight'],
+            price=config['product_element']['price'],
+            unit=config['product_element']['unit'],    
+        )
+
+        web_url = config['web_url']
+        return selector, element, web_url
 
     except FileNotFoundError:
         print(f'Error: the file {config_path} was not found')
@@ -35,4 +56,8 @@ def load_config(*, config_path: Union[str, Path]) -> ScraperSelector:
         raise
 
 
+if __name__ == "__main__":
+    selector, element, web = load_config(config_path='config.yaml')
+    print(selector.all_product_class)
+    
 
